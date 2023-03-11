9c39323402db9aaf326eb61e8a2661039e1ebd5c
shishir shetty <shishirshetty217@gmail.com>
2023-03-12 00:09:50 +0530

    minor changes in the homepage

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -7,7 +7,7 @@ export default function Homepage() {
     <p className="tracking-widest text-white text-sm md:text-base text-left font-medium mb-8 md:mb-12">NOW ADVERTISE YOUR PRODUCTS ON SOCIAL MEDIA WITH SPONSORLY</p>
     <h1 className="text-green-500 text-4xl md:text-6xl lg:text-8xl font-bold text-left mb-6 md:mb-12">Ready To Connect?</h1>
     <p className="text-white text-sm md:text-base text-left tracking-widest font-normal mb-6 md:mb-12">Boost your social media career with us Unleash the influencer in you Make the best of your content</p>
-    <button className="border-2 border-green-500 text-green-500 px-6 py-2 text-xs md:text-sm font-bold rounded-full hover:bg-black justify-center block">Let's Connect</button>
+    <button className="border-2 border-green-500 text-green-500 px-6 py-2 text-xs md:text-sm font-bold rounded-full hover:text-green-100 justify-center block">Let's Connect</button>
   </div>
 </div>
 
