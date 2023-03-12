3fd5b1f1a6f4a09993b6dae47c6b5ef8f7c8ef8c
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-12 12:29:29 +0530

    changes in login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,47 +1,9 @@
-import React, { useState } from 'react';
-import { GoogleLogin } from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
+import React from 'react'
 
-const CLIENT_ID = '904762651204-f7592jsjl3r1d02fhb95g2odu3o80nav.apps.googleusercontent.com';
-
-function Login() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const onSuccess = (response) => {
-  
-    console.log(response);
-    setLoggedIn(true);
-  };
-
-  const onFailure = (response) => {
-
-    console.log(response);
-  };
-
-  const logout = () => {
-    setLoggedIn(false);
-  };
-
+const Login = () => {
   return (
-    <div>
-      {loggedIn ? (
-        <GoogleLogout
-          clientId={904762651204-f7592jsjl3r1d02fhb95g2odu3o80nav.apps.googleusercontent.com}
-          buttonText="Logout"
-          onLogoutSuccess={logout}
-        />
-      ) : (
-        <GoogleLogin
-          clientId={904762651204-f7592jsjl3r1d02fhb95g2odu3o80nav.apps.googleusercontent.com}
-          buttonText="Login with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          responseType="code,token"
-        />
-      )}
-    </div>
-  );
+    <div>Login</div>
+  )
 }
 
-export default Login;
+export default Login
