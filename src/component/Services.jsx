e1b571521c5042b0a943e788380122e04455652d
shishir shetty <shishirshetty217@gmail.com>
2023-03-16 23:31:31 +0530

    added the service and changed the color

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import icon1 from '/icon1.png'
+import {FaRegListAlt} from 'react-icons/fa'
+import { FiCheckCircle } from "react-icons/fi";
+import { HiGlobeAlt } from "react-icons/hi";
+
 const Services = () => {
   return (
     <div class="mx-auto max-w-4xl">
@@ -8,20 +11,27 @@ const Services = () => {
       <h1 class='text-3xl font-bold'>How to Start</h1>
     </div>
     <div class='grid grid-cols-1 sm:grid-cols-3 gap-8'>
-      <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
-        <h1 class='text-lg font-bold mb-2'>Registration</h1>
-        <p class='text-gray-600 text-center'>Register into our site by giving your information</p>
+      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
+      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+  <FaRegListAlt size={30} className="text-black" />
+</div>
+
+        <h1 class='text-lg font-bold mb-2 text-white' >Registration</h1>
+        <p class='text-gray-400 text-center'>Register into our site by giving your information</p>
       </div>
-      <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
-        <h1 class='text-lg font-bold mb-2'>Complete Profile</h1>
-        <p class='text-gray-600 text-center'>Complete your profile to activate your account</p>
+      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
+      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+  <FiCheckCircle size={30} className="text-black" />
+</div>
+        <h1 class='text-lg font-bold mb-2 text-white'>Complete Profile</h1>
+        <p class='text-gray-400 text-center'>Complete your profile to activate your account</p>
       </div>
-      <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
-        <h1 class='text-lg font-bold mb-2'>Connect</h1>
-        <p class='text-gray-600 text-center'>You are now ready to start! Send one to one collab requests and more.</p>
+      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
+      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+  <HiGlobeAlt size={30} className="text-white" />
+</div>
+        <h1 class='text-lg font-bold mb-2 text-white'>Connect</h1>
+        <p class='text-gray-400 text-center'>Ready for collabs. DM for more. Keep it concise..</p>
       </div>
     </div>
   </div>
