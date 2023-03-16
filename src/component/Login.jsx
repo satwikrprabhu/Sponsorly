21983a464f5a9cd0153dea695d83d95d657f79f6
shishir shetty <shishirshetty217@gmail.com>
2023-03-17 00:05:35 +0530

    login added

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,7 +2,59 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className=''><div className="bg-primary " >
+    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+        <div>
+          <h2 className="text-3xl font-extrabold text-green-500 sm:text-4xl">
+            Login
+          </h2>
+          <div className="mt-8 sm:max-w-lg">
+            <form className="space-y-6">
+              <div>
+                <label htmlFor="email" className="block text-sm font-semibold text-green-500">
+                  Email
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="password" className="block text-sm font-semibold text-green-500">
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  </div>
   )
 }
 
--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -5,37 +5,38 @@ import { HiGlobeAlt } from "react-icons/hi";
 
 const Services = () => {
   return (
-    <div class="mx-auto max-w-4xl">
+    <div class="mx-auto max-w-6xl">
   <div class='sm:flex sm:flex-col sm:items-center flex-col'>
     <div class='text-white px-4 py-8 text-center sm:text-left'>
       <h1 class='text-3xl font-bold'>How to Start</h1>
     </div>
     <div class='grid grid-cols-1 sm:grid-cols-3 gap-8'>
-      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
-      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
-  <FaRegListAlt size={30} className="text-black" />
-</div>
-
+      <div class='flex flex-col items-center sm:rounded-lg  p-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+          <FaRegListAlt size={30} className="text-black" />
+        </div>
         <h1 class='text-lg font-bold mb-2 text-white' >Registration</h1>
         <p class='text-gray-400 text-center'>Register into our site by giving your information</p>
       </div>
-      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
-      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
-  <FiCheckCircle size={30} className="text-black" />
-</div>
+      <div class='flex flex-col items-center sm:rounded-lg  p-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+          <FiCheckCircle size={30} className="text-black" />
+        </div>
         <h1 class='text-lg font-bold mb-2 text-white'>Complete Profile</h1>
         <p class='text-gray-400 text-center'>Complete your profile to activate your account</p>
       </div>
-      <div class='flex flex-col items-center  rounded-lg shadow-lg p-8'>
-      <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
-  <HiGlobeAlt size={30} className="text-white" />
-</div>
+      <div class='flex flex-col items-center sm:rounded-lg:last-child  p-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+          <HiGlobeAlt size={30} className="text-black" />
+        </div>
         <h1 class='text-lg font-bold mb-2 text-white'>Connect</h1>
         <p class='text-gray-400 text-center'>Ready for collabs. DM for more. Keep it concise..</p>
       </div>
     </div>
   </div>
 </div>
+
+
 
   )
 }
