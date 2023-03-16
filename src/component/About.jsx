520db760a3d396eeec2e74f86ee2924a2c96abf0
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-17 00:20:15 +0530

    responsive

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -3,13 +3,13 @@ import Frame from '/Frame.png'
 
 const About = () => {
   return (
-    <div className='mt-8 md:mt-32 mx-auto flex flex-col md:flex-row items-center'>
+    <div className=' max-w-6xl mt-8 md:mt-32 mx-auto flex flex-col md:flex-row items-center'>
   <div className='md:w-1/4 flex justify-center'>
-  <img src={Frame} alt="" className='md:ml-24 mx-auto mt-6 md:my-0 mb-6 w-1/2 md:w-full shadow-lg shadow-indigo-500/200' />
+  <img src={Frame} alt="" className='md:ml-18 mx-auto mt-6 md:my-0 mb-6 w-1/2 md:w-full shadow-lg shadow-indigo-500/200' />
   </div>
   <div className='md:w-3/4 px-4 py-8 md:px-20 md:py-10 flex flex-col justify-center'>
   <h1 className='text-green-500 text-4xl md:text-6xl text-center font-semibold md:text-left font-Codec'>About Us</h1>
-    <p className='text-white text-lg tracking-widest my-8 md:my-4 font-semibold text-justify md:text-left md:pr-14'>
+    <p className='text-white text-lg tracking-widest my-8 md:my-4 font-semibold text-justify md:text-left'>
     Sponsorly is an innovative platform that aims to bridge the gap between content creators and businesses looking to promote their products or services. With its user-friendly interface, Sponsorly makes it easy for people to connect with influencers and other content creators, and to create effective marketing campaigns that reach a wider audience.Sponsorly provides a range of features and tools to help you achieve your goals.
     </p>
 </div>
--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -5,28 +5,28 @@ import { HiGlobeAlt } from "react-icons/hi";
 
 const Services = () => {
   return (
-    <div class="mx-auto max-w-6xl">
+    <div class="mx-auto max-w-9xl">
   <div class='sm:flex sm:flex-col sm:items-center flex-col'>
-    <div class='text-white px-4 py-8 text-center sm:text-left'>
-      <h1 class='text-3xl font-bold'>How to Start</h1>
+    <div class='text-white px-4 py-8 text-left sm:text-left'>
+      <h1 class='text-3xl font-bold text-left'>Services</h1>
     </div>
     <div class='grid grid-cols-1 sm:grid-cols-3 gap-8'>
-      <div class='flex flex-col items-center sm:rounded-lg  p-8'>
-        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+      <div class='flex flex-col items-center sm:rounded-lg  py-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300 mb-5">
           <FaRegListAlt size={30} className="text-black" />
         </div>
         <h1 class='text-lg font-bold mb-2 text-white' >Registration</h1>
         <p class='text-gray-400 text-center'>Register into our site by giving your information</p>
       </div>
-      <div class='flex flex-col items-center sm:rounded-lg  p-8'>
-        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+      <div class='flex flex-col items-center sm:rounded-lg  py-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300 mb-5">
           <FiCheckCircle size={30} className="text-black" />
         </div>
         <h1 class='text-lg font-bold mb-2 text-white'>Complete Profile</h1>
         <p class='text-gray-400 text-center'>Complete your profile to activate your account</p>
       </div>
-      <div class='flex flex-col items-center sm:rounded-lg:last-child  p-8'>
-        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300">
+      <div class='flex flex-col items-center sm:rounded-lg:last-child  py-8'>
+        <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center hover:bg-green-300 mb-5">
           <HiGlobeAlt size={30} className="text-black" />
         </div>
         <h1 class='text-lg font-bold mb-2 text-white'>Connect</h1>
