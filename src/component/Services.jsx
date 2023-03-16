8789e2bfb17b4d0f4b8d895c9d02158498a701c6
shishir shetty <shishirshetty217@gmail.com>
2023-03-16 20:13:31 +0530

    changes in whole image

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import icon1 from '/icon1.png'
 const Services = () => {
   return (
     <div class='sm:flex sm:flex-col sm:items-center flex-col'>
@@ -8,17 +8,17 @@ const Services = () => {
     </div>
     <div class='grid grid-cols-1 sm:grid-cols-3 gap-8'>
       <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src='./images/icon1.png' alt='' class='w-20 h-20 mb-4' />
+        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
         <h1 class='text-lg font-bold mb-2'>Registration</h1>
         <p class='text-gray-600 text-center'>Register into our site by giving your information</p>
       </div>
       <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src='./images/icon1.png' alt='' class='w-20 h-20 mb-4' />
+        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
         <h1 class='text-lg font-bold mb-2'>Complete Profile</h1>
         <p class='text-gray-600 text-center'>Complete your profile to activate your account</p>
       </div>
       <div class='flex flex-col items-center bg-white rounded-lg shadow-lg p-8'>
-        <img src='./images/icon1.png' alt='' class='w-20 h-20 mb-4' />
+        <img src={icon1} alt='' class='w-20 h-20 mb-4' />
         <h1 class='text-lg font-bold mb-2'>Connect</h1>
         <p class='text-gray-600 text-center'>You are now ready to start! Send one to one collab requests and more.</p>
       </div>
