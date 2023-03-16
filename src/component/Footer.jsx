b399cdc42dffe037ab5dc61bfc2674b4cfc00698
prathamasj <113054442+prathamasj@users.noreply.github.com>
2023-03-17 00:27:00 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -12,9 +12,9 @@ export default function Footer() {
           <h6 className='font-bold text-gray-300'>Solutions</h6>
           <ul>
             <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://influencermarketinghub.com/'target="blank">Resources</Link></li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://klear.com/'target="blank">Influencer Marketing</Link></li>
+            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://klear.com/'target="blank">Marketing</Link></li>
             <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://later.com/blog/'target="blank">Blogs</Link></li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://influencermarketingassociation.org/'target="blank">Industrial Associations</Link></li>
+
           </ul>
         </div>
 
@@ -22,19 +22,19 @@ export default function Footer() {
           <h6 className='font-bold text-gray-300 '>Company</h6>
           <ul>
             <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://www.upfluence.com/pricing/'target="blank">Pricing</Link></li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://developer.twitter.com/en/docs'target="blank">Documentation</Link></li>
+            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to='https://developer.twitter.com/en/docs'target="blank">Docs</Link></li>
             <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to=' https://blog.hubspot.com/marketing/'target="blank">Guides</Link></li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to=''target="blank">Analysis</Link></li>
+          
           </ul>
         </div>
 
         <div>
           <h6 className='text-gray-300 font-bold'>Research</h6>
           <ul>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'>Info</li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'>Creation</li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'>Innovate</li>
-            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'>Ideas</li>
+           <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to=' https://www.kickstarter.com/'target="blank">Creation</Link></li>
+            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to=' https://socialblade.com/'target="blank">Innovate</Link></li>
+            <li className='py-2 text-sm hover:text-green-500 cursor-pointer'><Link to=' https://adage.com/'target="blank">Ideas</Link></li>
+
           </ul>
         </div>
         </div>
