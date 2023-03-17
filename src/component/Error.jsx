6076e86a4d307f72aeaa183a0ebd92d49bfccad8
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-17 14:53:20 +0530

    Update Error.jsx

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>This is Error Page</div>
+    <div>
+      <h1 className=' text-green-500 text-8xl text-center font-bold pt-[15rem]'>Error 404</h1>
+      <h1 className='text-green-500 text-4xl pt-4 text-center font-bold'>Page Not Found!!</h1>
+    </div>
   )
 }
 
