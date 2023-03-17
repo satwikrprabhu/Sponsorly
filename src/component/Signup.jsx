412a58055fa371d45c454fad84c127e9a4bbc924
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-17 17:41:11 +0530

    Update Signup.jsx

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -91,7 +91,7 @@ const Signup = () => {
             <div>
               <button
                 type="submit"
-                className="inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                className="inline-flex items-center px-5 py-2 border border-transparent text-base font-semibold rounded-full text-black bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
               >
                 Submit
               </button>
