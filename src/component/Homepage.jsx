0d35a30524fbc55994ed5aa2e2b3bad071039a5a
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-17 10:06:06 +0530

    Update Homepage.jsx

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -4,7 +4,7 @@ export default function Homepage() {
   return (<>
         <div className="h-screen w-full bg bg-no-repeat flex mt-16 md:mt-32 justify-center" style={{backgroundImage: `url('https://www.futureswap.com/images/Graphic-Blue-Wave.svg'), url('https://www.futureswap.com/images/Graphic-Purple-Wave.svg')`}}>
   <div className="mx-auto max-w-3xl md:max-w-5xl lg:max-w-7xl md:px-8 lg:px-16 md:py-12 ml-4 ">
-    <p className="tracking-widest text-green-0 text-[1.1rem] md:text-base text-left font-semibold mb-12 md:mb-12">NOW ADVERTISE YOUR PRODUCTS ON SOCIAL MEDIA WITH SPONSORLY</p>
+    <p className="tracking-widest text-green-0 text-[1.1rem] pr-4 md:text-base text-left font-semibold mb-12 md:mb-12">NOW ADVERTISE YOUR PRODUCTS ON SOCIAL MEDIA WITH SPONSORLY</p>
     <h1 className="text-white text-6xl md:text-6xl lg:text-8xl font-bold text-left mb-12 md:mb-12 font-Codec">Ready To Connect?</h1>
     <p className=" text-gray-400 text-[1.1rem] pr-10 md:text-base text-left tracking-widest font-normal mb-16 md:mb-12">Boost your social media career with us Unleash the influencer in you. <span>Make the best of your content</span>
    </p>
