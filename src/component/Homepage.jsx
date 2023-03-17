f6b9fca4e1646062857b990b965c16559c788ab2
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-17 10:13:00 +0530

    updated button

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Homepage() {
   return (<>
         <div className="h-[75vh] md:h-screen w-full bg bg-no-repeat flex mt-16 md:mt-32 justify-center" style={{backgroundImage: `url('https://www.futureswap.com/images/Graphic-Blue-Wave.svg'), url('https://www.futureswap.com/images/Graphic-Purple-Wave.svg')`}}>
@@ -8,7 +8,8 @@ export default function Homepage() {
     <h1 className="text-white text-6xl md:text-6xl lg:text-8xl font-bold text-left mb-12 md:mb-12 font-Codec">Ready To Connect?</h1>
     <p className=" text-gray-400 text-[1.1rem] pr-10 md:text-base text-left tracking-widest font-normal mb-16 md:mb-12">Boost your social media career with us Unleash the influencer in you. <span>Make the best of your content</span>
    </p>
-    <button className="border-2 border-green-500 text-green-500 px-6 py-2 text-[1rem] md:text-sm font-bold rounded-full hover:text-green-300 hover:border-green-300 justify-center block">Let's Connect</button>
+
+     <Link to='login' className='font-Codec'><button className="border-2 border-green-500 text-green-500 px-6 py-2 text-[1rem] md:text-sm font-bold rounded-full hover:text-green-300 hover:border-green-300 justify-center block">Let's Connect</button></Link>
   </div>
 </div>
     </>
